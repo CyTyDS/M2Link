9005a65e0bc63743708861ec64b7868cb053dff7
CyTyDS <ds1997@hotmail.fr>
2019-12-04 15:56:35 +0100

    Ajoutez des fichiers projet.

---
+++ M2Link/Controllers/RegisterController.cs
@@ -0,0 +1,36 @@
+﻿using M2Link.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M2Link.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Register/Form
+        [HttpGet]
+        public ActionResult Form()
+        {
+            return View("Form");
+        }
+
+        // Post: Register/Form
+        [HttpPost]
+        public ActionResult Form(RegisterModel rm)
+        {
+            if (rm.Mdp.Length < 8)
+            {
+                return View("Form", rm);
+            }
+            return View("Form");
+        }
+    }
+}
---
+++ M2Link/Models/RegisterModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace M2Link.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [DisplayName("Nom")]
+        public string Nom { get; set; }
+
+        [Required]
+        [DisplayName("Prenom")]
+        public string Prenom { get; set; }
+
+        [Required]
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("Pseudo")]
+        public string Pseudo { get; set; }
+
+        [Required]
+        [DisplayName("Mot de passe")]
+        public string Mdp { get; set; }
+
+        [Required]
+        [DisplayName("Rentrez à nouveau votre mot de passe")]
+        public string VerifMDP { get; set; }
+        
+    }
+}
