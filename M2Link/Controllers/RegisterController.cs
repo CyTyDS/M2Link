3f31645a4629289c21d30573ef072eb5c9eaeefe
CyTyDS <ds1997@hotmail.fr>
2019-12-11 16:32:45 +0100

    finished before DB, ready for next TP

--- M2Link/Controllers/RegisterController.cs
+++ M2Link/Controllers/RegisterController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -26,11 +27,40 @@ namespace M2Link.Controllers
         [HttpPost]
         public ActionResult Form(RegisterModel rm)
         {
-            if (rm.Mdp.Length < 8)
+            if (rm != null)
             {
-                ModelState.AddModelError("Mdp", "Le mot de passe est trop faible");
-                return View("Form", rm);
+                if (rm.Mdp != null)
+                {
+                    if (rm.Mdp.Length < 8)
+                    {
+                        ModelState.AddModelError("Mdp", "Le Mot de passe entré fait moins de 8 caractères.");
+                        return View("Form");
+                    }
+
+                    Regex r = new Regex(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
+                    if (!r.IsMatch(rm.Mdp))
+                    {
+                        ModelState.AddModelError("Mdp", "Le Mot de passe entré n'est pas assez sécurisé.\n" +
+                            " Pensez à utiliser au moins une majuscule, une minuscule, un chiffre et un caratères spécial.");
+                        return View("Form");
+                    }
+
+                    if (rm.VerifMDP != null)
+                    {
+                        if (!rm.Mdp.Equals(rm.VerifMDP))
+                        {
+                            ModelState.AddModelError("VerifMDP", "Le Mot de passe n'a pas été réécrit correctement.");
+                            return View("Form");
+                        }
+                    }
+                }
             }
+            
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
             return View("Form");
         }
     }
