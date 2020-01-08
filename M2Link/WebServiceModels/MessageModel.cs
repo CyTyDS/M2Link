2adb16aaae2cbdd3ef66cc9177636cf18ee83a6e
Ushelajyan <57437593+Ushelajyan@users.noreply.github.com>
2020-01-08 16:42:24 +0100

    Tp3 (#2)
    * feat: did login validate -> Création de votre méthode « Validate >> finished
    
    * WIP
    
    * feat: finished web service missing : Utilisation des Services Web dans un projet Console
    
    * feat : finished TP3 verified webService work

---
+++ M2Link/WebServiceModels/MessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace M2Link.WebServiceModels
+{
+    public class MessageModel
+    {
+        public string User { get; set; }
+        public string Content { get; set; }
+        public Guid UserId { get; set; }
+        public Guid MessageId { get; set; }
+    }
+}
